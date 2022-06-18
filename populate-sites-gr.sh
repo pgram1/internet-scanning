faed0195f0b0231bee6e4d03c2a7e7ec5cd6de22
Petros Grammatikopoulos <pgram1.git@gmail.com>
2022-06-19 02:05:55 +0300

    Add files via upload

--- populate-sites-gr.sh
+++ populate-sites-gr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-while : ; do
+#while : ; do
 zmap -p 80 --whitelist-file ipranges.csv -o 80.txt
 zmap -p 443 --whitelist-file ipranges.csv -o 443.txt
 zmap -p 8000 --whitelist-file ipranges.csv -o 8000.txt
 zmap -p 8080 --whitelist-file ipranges.csv -o 8080.txt
-done
+#done
